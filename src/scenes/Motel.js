b8072d74296db87cad8ee821767af8f9a68c39a0
Anna <annaschultz10@gmail.com>
2023-06-12 16:39:34 -0700

    finally got the zoom ins to work with the piece of shit overlap system. Thanks for nothing arcade physics

--- src/scenes/Motel.js
+++ src/scenes/Motel.js
@@ -218,6 +218,7 @@ class Motel extends Phaser.Scene {
         });
 
         this.boxBundle = new dialogBoxBundle(this, ['bottom3', ""], ['end', "talk"]);
+        this.boxBundle1 = new dialogBoxBundle(this, ['bottom3', ""], ['end', "talk"]);
 
 
     }
@@ -365,7 +366,7 @@ class Motel extends Phaser.Scene {
         } else if (selection === "paintingS"){
             console.log("dialogue paintingS");
             if(paintingS) {this.paintingSInspect.hide(); };
-            this.boxBundle = new dialogBoxBundle(this, [
+            this.boxBundle1 = new dialogBoxBundle(this, [
                 //['sound', "audio22"],
                 ['bottom3', "Hello"],
                 //['sound', "audio23"],
@@ -529,11 +530,15 @@ class Motel extends Phaser.Scene {
         this.Marion.setVelocity(150 * this.direction.x, 150 * this.direction.y);
 
         //updating text
-        this.boxBundle.update();
-        if(!(this.boxBundle.nextInstruction === 'hide')){
-            this.deskCloseUp.setAlpha(0);
-        } else {
-            this.deskCloseUp.setAlpha(1);
+        this.boxBundle1.update();
+        if(this.boxBundle){
+            this.boxBundle.update();
+            if(!(this.boxBundle.nextInstruction === 'hide')){
+                this.deskCloseUp.setAlpha(0);
+            } else {
+                this.deskCloseUp.setAlpha(1);
+         }
         }
+        
     }
 }
